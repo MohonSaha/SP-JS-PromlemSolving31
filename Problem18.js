f6047cbea5ee75ff1ca77829d5653f6df6de129e
MohonSaha <practicewithmohon@gmail.com>
2022-12-11 11:51:14 +0600

    adding more problem

--- Problem18.js
+++ Problem18.js
@@ -1,4 +1,4 @@
-// Check if a number is positive negative or zero
+// Check if a number is positive negative or zero by built-in sign() method
 
 function checkNumber (num){
     const check = Math.sign(num)
@@ -6,3 +6,24 @@ function checkNumber (num){
 }
 
 console.log(checkNumber(-5));
+
+
+
+
+
+// Check if a number is positive negative or zero by manual function
+
+function checkNumber (num){
+    if(num > 0){
+        console.log("This number is positive.");
+    }
+    else if(num === 0){
+        console.log("This is Zero");
+    }
+    else{
+        console.log("Negative");
+    }
+ 
+}
+
+console.log(checkNumber(9));
---
+++ Problem19.js
@@ -0,0 +1,12 @@
+// get a random number between 0 - 1 
+var x = Math.random();
+// console.log(x);
+
+
+// get a four digit random number 
+var x = Math.random();
+x = x * 10000;
+// console.log(Math.floor(x));
+
+
+// 
